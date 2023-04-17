d098c566fccadfa2280cabddb5981025302e613f
Daniel Loredo <danjorloredo@gmail.com>
2023-04-17 10:20:27 -0600

    Added deportes button

--- src/app/deportes/deportes.component.ts
+++ src/app/deportes/deportes.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-deportes',
   templateUrl: './deportes.component.html',
-  styleUrls: ['./deportes.component.css']
+  styleUrls: ['./deportes.component.css'],
 })
 export class DeportesComponent {
+  constructor(private router: Router) {}
 
+  onSelectDeporte() {
+    this.router.navigate(['/deportes/futbol']);
+  }
 }
