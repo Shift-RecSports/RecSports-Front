5ebc45d57438ea0d5ca445e7132e29eb8ec204d8
martnreh <martinbarrientos451@gmail.com>
2023-04-28 10:35:12 -0600

    me equivoque pero ahi va api service a main

--- src/app/classes/deportes.ts
+++ src/app/classes/deportes.ts
@@ -17,3 +17,22 @@ export class newDeporte {
   imagen: string;
   descripcion: string;
 }
+
+
+export class Deporte {
+  id: string;
+  nombre: string;
+  descripcion: string;
+  materiales: string;
+  imagen: string;
+  duracion: number;
+
+  constructor(id: string, nombre: string, descripcion: string, materiales: string, imagen: string, duracion: number) {
+    this.id = id;
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.materiales = materiales;
+    this.imagen = imagen;
+    this.duracion = duracion;
+  }
+}
