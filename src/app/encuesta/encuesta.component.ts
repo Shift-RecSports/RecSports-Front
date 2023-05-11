f7b4bd2982c5e98929b78a4d74c891e557203893
David Martínez Celis <davidmcelisg@gmail.com>
2023-05-10 19:57:55 -0600

    encuesta alumnos semiterminada

--- src/app/encuesta/encuesta.component.ts
+++ src/app/encuesta/encuesta.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NzMarks } from 'ng-zorro-antd/slider';
+
 
 @Component({
   selector: 'app-encuesta',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./encuesta.component.css']
 })
 export class EncuestaComponent {
+  tickValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+  formatLabel(value: number): string {
+    if (value >= 1000) {
+      return value.toString();
+    }
+
+    return `${value}`;
+  }
 }
+
+
