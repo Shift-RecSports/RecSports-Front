531e25cbe36348a06c9cb24b2fb37abd3ad0a4c4
DanielLoredo <danjorloredo@gmail.com>
2023-06-05 09:00:19 -0600

    Reservacion cancelada desde admin

--- src/app/deportes/deporte-seleccionado/modal-reservacion/modal-reservacion.component.ts
+++ src/app/deportes/deporte-seleccionado/modal-reservacion/modal-reservacion.component.ts
@@ -3,6 +3,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/service/api.service';
 import { AuthService } from 'src/app/service/auth.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
 
 interface reservacionType {
   reservacion: {
@@ -48,6 +49,7 @@ export class ModalReservacionComponent {
     private _apiService: ApiService,
     private service: AuthService,
     private router: Router,
+    private notification: NzNotificationService,
     @Inject(MAT_DIALOG_DATA) public data: reservacionType
   ) {}
 
@@ -78,23 +80,33 @@ export class ModalReservacionComponent {
     if (this.service.isLoggedIn() && this.service.GetUserRole() == 'ADMIN') {
       const url = `/reservaciones/${this.data.reservacion.id}`;
 
-      const body = {
-        hora_seleccionada: this.data.reservacion.hora_seleccionada,
-        matricula_alumno: null,
-        fecha: this.data.reservacion.fecha,
-        espacio: this.data.reservacion.espacio,
-        estatus: 1,
-      };
+      this._apiService.delete(url).subscribe(
+        (data) => {
+          const type = 'success';
+          const title = `Se cancelo la reservacion con exito`;
+          const description = `Se ha cancelado con exito`;
 
-      this._apiService.put(url, body).subscribe((data) => {
-        this.onNoClick();
-        window.location.reload();
-      });
+          this.createNotification(type, title, description);
+          this.onNoClick();
+        },
+        (e) => {
+          const type = 'error';
+          const title = `No se ha logrado cancelar la reservacion`;
+          const description = e.error.message;
+
+          this.createNotification(type, title, description);
+          this.onNoClick();
+        }
+      );
     }
   }
 
   onMisReservacionesClick() {
     this.router.navigate(['/reservaciones']);
     this.dialogRef.close();
+  }
+
+  createNotification(type: string, title: string, description: string): void {
+    this.notification.create(type, title, description);
   }
 }
